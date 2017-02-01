1797d6f7e527191f047120af19b03758909430a8
hullale <hullale@oregonstate.edu>
2017-02-01 13:55:03 -0800

    Some preliminary code
    Not sure if anything works yet.

--- src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
+++ src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
@@ -4,4 +4,24 @@ package edu.oregonstate.cs361.battleship;
  * Created by michaelhilton on 1/26/17.
  */
 public class BattleshipModel {
+  String name;
+  int length; 
+  int startAcross;
+  int startDown;
+  int endAcross;
+  int endDown;
+  //format: [startAcross, startDown, endAcross, endDown]
+  int[] coordinates;
+  
+  private static String getName(){
+    return name; 
+  }
+  private static int getLength(){
+    return length;
+  }
+  private static int[] getCoordinates(){
+    coordinates = new int[]{startAcross, startDown, endAcross, endDown};
+    return coordinates;
+  }
+    
 }
