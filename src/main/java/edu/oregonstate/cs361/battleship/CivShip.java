f939401ccdbc48d4e5a856f70773f5c98a399187
sprouseamark <sprouseamark@gmail.com>
2017-03-01 12:50:23 -0800

    Added civhit logic

--- src/main/java/edu/oregonstate/cs361/battleship/CivShip.java
+++ src/main/java/edu/oregonstate/cs361/battleship/CivShip.java
@@ -14,8 +14,19 @@ public class CivShip extends Ship {
 
     }
 
-    public void hit()//Occures when a civilian ship is shot
+    public void hit()//Occurs when a civilian ship is shot
     {
+        //Need to find which orientation the ship is in
+        if(start.get_x() == end.get_x())//If the X's are equal, then it is vertical
+        {
 
+            //Iterate through the ships, places and create a hit at that location
+            //This might have to change locations/scopes
+
+        }
+        else//Not vertical means Horizontal
+        {
+
+        }
     }
 }
