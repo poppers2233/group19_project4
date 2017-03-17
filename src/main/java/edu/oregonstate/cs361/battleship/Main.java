b49dac8be06874bd78b91029bcea685809d6bc4b
sprouseamark <sprouseamark@gmail.com>
2017-03-17 10:37:40 -0700

    Refactored code and added Fixed Previous refactor errors

--- src/main/java/edu/oregonstate/cs361/battleship/Main.java
+++ src/main/java/edu/oregonstate/cs361/battleship/Main.java
@@ -11,6 +11,8 @@ import static spark.Spark.staticFiles;
 
 public class Main {
 
+    private static Game game_manager = new Game();
+
     public static void main(String[] args) {
 
 
@@ -27,5 +29,74 @@ public class Main {
 
 
     }
+
+    //This function should return a new model
+    public static String newModel() {
+
+        BattleshipModel model = new BattleshipModel();
+        //Ship ship = new Ship("?",1,0,0,0,0);
+        Gson gson = new Gson();
+        //System.out.println(gson.toJson(ship));
+        //System.out.println(gson.toJson(model));
+        return gson.toJson(model);
+    }
+
+    //This function should accept an HTTP request and deseralize it into an actual Java object.
+    private static BattleshipModel getModelFromReq(Request req){
+        String boardState = req.body(); //should be a JSON stored as a string
+        Gson gson = new Gson();
+        return gson.fromJson(boardState, BattleshipModel.class); //returns the battleship model
+    }
+
+    private static String prepFire(Request req)
+    {
+        BattleshipModel model = getModelFromReq(req);
+        if(model == null){
+            model = new BattleshipModel();
+        }
+        Gson gson = new Gson();
+        int[] pos = new int[]{0, 0};                                //to be passed to the position helper function
+        pos[0] = Integer.parseInt(req.params(":col"));
+        pos[1] = Integer.parseInt(req.params(":row"));
+        Coord shot = new Coord(pos[0], pos[1]);
+
+        game_manager.prepFire(model, shot);
+
+        return gson.toJson(model);
+    }
+
+    private static String scan(Request req)
+    {
+
+        BattleshipModel currModel = getModelFromReq(req);
+        String row = req.params("row");
+        String col = req.params("col");
+        int rowInt = Integer.parseInt(row);
+        int colInt = Integer.parseInt(col);
+        game_manager.scan(currModel, rowInt,colInt);
+        Gson gson = new Gson();
+        return gson.toJson(currModel);
+    }
+
+    private static String placeShip(Request req)
+    {
+        BattleshipModel model = getModelFromReq(req); //calls above function to create an object from board state
+        if(model == null){
+            model = new BattleshipModel();
+        }
+        Gson gson = new Gson();
+
+        //declares variables for the details specified for the ship
+        String id = req.params(":id");
+        int row = Integer.parseInt(req.params(":row"));
+        int col = Integer.parseInt(req.params(":col"));
+        String orientation = req.params(":orientation");
+        String difficulty = req.params(":difficulty");
+
+        game_manager.placeShip(model, new Coord(row, col), orientation, id,  difficulty);
+
+        return gson.toJson(model);
+    }
+
 }
 
